3f6efeee52213a971f01dbaca848d04bacb46a1f
Judith Tuyisingize <judithtuyisingize@Judiths-MacBook-Pro.local>
2023-05-17 12:31:44 -0400

    added

---
+++ scripts/final.js
@@ -0,0 +1,143 @@
+let maxPoints = 3000;
+
+// Do not change the import statement
+import placeholderQuestions from "./placeholder-questions.js";
+console.log({ placeholderQuestions });
+
+
+// function to generate a random category
+function generateCategory() {
+    const categories = ["History", "General", "Animals", "Mythology", "Computers", "Nature"];
+    const index = Math.floor(Math.random() * categories.length);
+    return categories[index];
+}
+
+// when the Final Round page loads
+function FinalRound() {
+    const category = generateCategory();
+    const categoryElement = document.getElementById("category");
+    const wagerElement = document.getElementById("wager");
+
+    // display the category on the page
+    categoryElement.textContent = `Category: ${category}`;
+
+    // prompt the player to make a wager up to their maximum point total
+    wagerElement.textContent = `Make a wager up to ${maxPoints}`;
+
+    // enable the "Submit Wager" button
+    const submitWager = document.getElementById("submit-wager");
+    submitWager.disabled = false;
+}
+
+// when the player submits their wager
+function submitWager() {
+    const wagerElement = document.getElementById("wager");
+    const wagerInput = document.getElementById("wager-input").value;
+
+    // check if the wager is valid
+    if (isNaN(wagerInput) || wagerInput < 0 || wagerInput > maxPoints) {
+        wagerElement.textContent = `Invalid wager. Make a wager up to ${maxPoints}`;
+        return;
+    }
+
+    // store the player's wager
+    const playerWager = parseInt(wagerInput);
+    // disable the "Submit Wager" button
+    const submitWager = document.getElementById("submit-wager");
+    submitWager.disabled = true;
+
+    // start the final round with the chosen category and wager
+    FinalRound(category, playerWager);
+}
+
+// Get the elements needed for the final round
+const categoryElement = document.getElementById('category');
+const wagerInput = document.getElementById('wager');
+const submitWagerBtn = document.getElementById('submit-wager');
+const questionElement = document.getElementById('question');
+const submitAnswerBtn = document.getElementById('submit-answer');
+
+// Show the category to the players
+categoryElement.innerText = "Category: ${category}";
+
+// When both players have made their wagers, show the question
+submitWagerBtn.addEventListener('click', () => {
+    const player1Wager = parseInt(document.getElementById('player1-wager').value);
+    const player2Wager = parseInt(document.getElementById('player2-wager').value);
+
+    // Check if both players have made their wagers
+    if (!isNaN(player1Wager) && !isNaN(player2Wager)) {
+        // Enable the answer submission button
+        submitAnswerBtn.disabled = false;
+
+        // Show the question to the players
+        questionElement.innerText = "./placeholder-questions.js";
+    }
+});
+
+// When a player submits their answer, disable the answer submission button
+submitAnswerBtn.addEventListener('click', () => {
+    submitAnswerBtn.disabled = true;
+
+    // Check if the answer is correct
+    const answer = document.getElementById('search-answer').value;
+    if (answer.toLowerCase() === " ") {
+        // Let the players that the answer is correct and add the wagered amount to the player's score
+        alert("Correct!");
+    } else {
+        // Let the players that the answer is incorrect and subtract the wagered amount from the player's score
+        alert("Incorrect.");
+    }
+});
+
+// Store the maximum wager for each player
+let maxWager = {
+    player1: player1Score,
+    player2: player2Score
+};
+
+// Randomly select a category for the final round
+let finalRoundCategory = categories[Math.floor(Math.random() * categories.length)];
+
+// Display the category and prompt each player to make a wager
+displayFinalRoundPrompt(finalRoundCategory, maxWager);
+
+// Store the wagers for each player
+let player1Wager = 0;
+let player2Wager = 0;
+
+// Listen for wagers from each player
+listenForWagers((player, wager) => {
+    if (player === "player1") {
+        player1Wager = wager;
+    } else if (player === "player2") {
+        player2Wager = wager;
+    }
+
+    // Check if both players have submitted their wagers
+    if (player1Wager !== 0 && player2Wager !== 0) {
+        // Both wagers have been submitted, reveal the final question
+        let finalQuestion = getFinalQuestion(finalRoundCategory);
+        displayFinalQuestion(finalQuestion);
+
+        // Listen for answers from each player
+        listenForAnswers((player, answer) => {
+            // Check if the answer is correct
+            let isCorrect = checkFinalAnswer(finalQuestion, answer);
+
+            // Update the scores based on the wagers and correctness of the answer
+            if (isCorrect) {
+                player1Score += player1Wager;
+                player2Score += player2Wager;
+            } else {
+                player1Score -= player1Wager;
+                player2Score -= player2Wager;
+            }
+
+            // Display the updated scores and notify the players of the winner
+            displayFinalScore(player1Score, player2Score);
+            notifyWinner(getWinner(player1Score, player2Score));
+        });
+    }
+});
+
--- scripts/round1.js
+++ scripts/round1.js
@@ -1,125 +1,208 @@
-// // Do not change the import statement
-// import placeholderQuestions from "./placeholder-questions.js";
-// console.log({ placeholderQuestions });
+// Do not change the import statement
+import placeholderQuestions from "./placeholder-questions.js";
+console.log({ placeholderQuestions });
 
 
-// // Fetch placeholder questions from an external source
-// let fetchPlaceholderQuestions = async () => {
-//     try {
-//         // Use fetch to make a GET request to the API endpoint
-//         let response = await fetch("./placeholder-questions.js");
-//         // Check if the response is successful
-//         if (!response.ok) {
-//             throw new Error('Error fetching questions');
-//         }
 
-//         // Parse the response into JSON
-//         let questions = await response.json();
 
-//         // Do whatever you need to do with the questions data
-//         let allQuestions = allQuestions.results;
-//         displayQuestions(allQuestions);
-//         console.log(allQuestions.results[0].category);
-//         questions.push(questions.value);
-//         clearDisplay();
-//         displayQuestions();
+// Get the 'Ready' button element
+// const readyButton = document.getElementById('ready-game-button');
 
-//     } catch (error) {
-//         // Handle any errors that occur during the fetch operation
-//         console.error(error);
+// Add an event listener to the button that redirects the player to the Round 1 page when clicked
+readyButton.addEventListener('click', () => {
+    window.location.href = 'round1.html';
+});
+
+window.addEventListener("load", function () {
+    // Get references to the notification and buttons in the round 1 html
+    let guessButton = document.getElementById("guess-button");
+    let passButton = document.getElementById("pass-button");
+    let nextRoundButton = document.getElementById("next-round-button");
+
+    // Display the notification for player 1's turn
+    notification.textContent = "It's Player 1's turn to choose.";
+
+    // Disable the buttons
+    guessButton.disabled = true;
+    passButton.disabled = true;
+    nextRoundButton.disabled = true;
+});
+
+
+// Add event listeners to all card elements
+
+populateCategory("card", ".nature", nature);
+populateCategory("card", ".animal", animal);
+populateCategory("card", ".computers", computers);
+populateCategory("card", ".mythology", mythology);
+populateCategory("card", ".history", history);
+populateCategory("card", ".general", general);
+
+function populateCategory(
+    elementCardId,
+    elementQuestionClass,
+    categoryFilteredArray
+) {
+    let elementCard = document.getElementById(elementCardId);
+
+    let card = categoryFilteredArray[0].category;
+    elementCard.textContent = card;
+
+    let questions = document.querySelectorAll(elementQuestionClass);
+    questions.forEach((question, index) => {
+        question.innerHTML = categoryFilteredArray[index].question;
+    });
+}
+
+let natureAnswers = document.querySelectorAll('.answer.nature');
+console.log(natureAnswers);
+
+
+for (let i = 0; i < natureAnswers.length; i++) {
+    natureAnswers[i].addEventListener('click', () => {
+        let answer = natureAnswers[i].value.toLowerCase();
+        let actualAnswer = nature[i].answer.toLocaleLowerCase();
+        if (answer === actualAnswer) {
+            natureAnswers[i].classList.add("correct");
+        } else {
+            console.log("it's not correct");
+        }
+    });
+}
+
+// let passButton = document.getElementById("pass-button");
+// let notification = document.getElementById("notification");
+
+// passButton.addEventListener("click", function () {
+//     currentPlayer = 2;
+//     notification.textContent = "Next, Player 2's turn";
+// });
+
+// // Get the player's answer from some input field
+// let playerAnswer = document.getElementById('search-answer').value;
+
+// // Check if the answer is correct
+// if (playerAnswer === getQuestion.answer) {
+//     // Award the player points
+//     scoreBoard[currentPlayer.name] += getQuestion.points;
+
+//     // Blank out the question card
+//     questionCard.innerHTML = '';
+
+//     // Update the notification area
+//     notification.textContent = `${currentPlayer.name}'s turn`;
+// } else {
+//     // Incorrect answer handling
+// }
+
+// // let's say firstPlayer is the player who attempted the answer
+// // let's say secondPlayer is the player who will get the next turn
+
+// if (answerIsIncorrect) {
+//     firstPlayer.score -= pointTotal;
+
+//     // swap current player with other player
+//     let temp = firstPlayer;
+//     firstPlayer = secondPlayer;
+//     secondPlayer = temp;
+
+//     // show notification that other player's turn has begun
+//     showNotification(`It's ${secondPlayer.name}'s turn to answer.`);
+
+//     if (secondPlayerAttemptsAnswer()) {
+//         // if the other player guesses correctly
+//         secondPlayer.score += pointTotal;
+//         blankOutCard();
+//         showNotification(`Correct answer by ${secondPlayer.name}.`);
+//     } else {
+//         // if the other player also guesses incorrectly
+//         showNotification(`No one guessed correctly. Choosing a new question.`);
+//         getNewQuestion(firstPlayer);
 //     }
-// };
+// }
 
+// function updateScore(player1Score, player2Score) {
+//     document.getElementById("player1Score").innerHTML = player1Score;
+//     document.getElementById("player2Score").innerHTML = player2Score;
+// }
 
+// // award points to player 1
+// player1Score += pointTotal;
+// updateScore(player1Score, player2Score);
 
-// // Populate game board with fetched questions and answers
-// function displayQuestions(categoriesToDisplay) {
-//     for (let i = 0; i < categories.length; i++) {
-//         let gameBoardDisplay = document.getElementById('game-board');
-//         categoriesToDisplay.forEach((category) => {
-//             console.log(category);
-//             // let pTag = document.createElement("p");
-//             // pTag.textContent = category.name;
-//             // gameBoardDisplay.appendChild(pTag);
+// // subtract points from player 2
+// player2Score -= pointTotal;
+// updateScore(player1Score, player2Score);
+
+// // select a new card for the question
+// if (selectedCard) {
+//     displayAlert("You cannot pick a new card until you answer or pass the current question.");
+// }
+
+// // Get the "Round 2" button element
+// let nextRoundButton = document.getElementById('next-round-button');
+
+// // Set initial score values for players 1 and 2
+// let player1Score = 0;
+// let player2Score = 0;
+
+// // Check if a player has won or if the board has been cleared
+
+// function checkWinConditions() {
+//     if (player1Score >= 15000 || player2Score >= 15000 || boardIsCleared()) {
+//         // Display a message to alert players to move on to Round 2
+//         alert("Congratulations, you are doing great. It's time to move on to Round 2!");
+
+//         // Enable the "Round 2" button
+//         nextRoundButton.disabled = false;
+
+//         // Pass score information to the Round 2 page using query parameters
+//         let urlParams = new URLSearchParams();
+//         urlParams.set('player1Score', player1Score);
+//         urlParams.set('player2Score', player2Score);
+//         const queryString = urlParams.toString();
+//         const round2Url = `round2.html?${queryString}`;
+
+//         // Navigate to the Round 2 page
+//         window.location.href = round2Url;
+//     }
+// }
+
+// // Check the win conditions whenever the score is updated
+// function updateScore(player, points) {
+//     if (player === 1) {
+//         player1Score += points;
+//     } else if (player === 2) {
+//         player2Score += points;
+//     }
+
+//     // Update the score display on the page
+//     updateScoreDisplay(player1Score, player2Score);
+
+//     // Check if either player has won or if the board has been cleared
+//     checkWinConditions();
+// }
+
+// // Get the score display elements for both players
+// const player1ScoreDisplay = document.getElementById('player1Score');
+// const player2ScoreDisplay = document.getElementById('player2Score');
+
+// // Update the score display on the page
+// function updateScoreDisplay(player1Score, player2Score) {
+//     player1ScoreDisplay.textContent = `Player 1 Score: ${player1Score}`;
+//     player2ScoreDisplay.textContent = `Player 2 Score: ${player2Score}`;
+// }
+
+// // Determine if the board has been cleared
+// function boardIsCleared() {
+//     let boardCleared = true;
+//     board.forEach((row) => {
+//         row.forEach((card) => {
+//             if (card.question !== "") {
+//                 boardCleared = false;
+//             }
 //         });
-//     }
-// }
-
-
-// for (let j = 0; j < answer.length; j++) {
-//     let answerDiv = document.getElementById('div');
-//     answerDiv.forEach((answer) => {
-//         answerDiv.textContent = answer[i];
-//         console.log(answer);
-//     })
-
-//     // gameBoardDisplay.appendChild(answerDiv);
-
-// }
-
-
-
-// // Add event listeners to handle user interactions
-// const questionTiles = document.getElementsByClassName('question');
-// for (let i = 0; i < questionTiles.length; i++) {
-//     questionTiles[i].addEventListener('click', () => {
-
 //     });
+//     return boardCleared;
 // }
 
-
-
-// let getNature = placeholderQuestions.filter((cat) => cat.category === "Nature").slice(5);
-// let getAnimals = placeholderQuestions.filter((cat) => cat.category === "Animals").slice(5);
-// let getComputers = placeholderQuestions.filter((cat) => cat.category === "Computers").slice(5);
-// let getMythology = placeholderQuestions.filter((cat) => cat.category === "Mythology").slice(5);
-// let getHistory = placeholderQuestions.filter((cat) => cat.category === "History").slice(5);
-// let getGeneral = placeholderQuestions.filter((cat) => cat.category === "General").slice(5);
-
-// // a function that will grab the Nature Section
-// populateCategory("nature-title", "nature-list", getNature);
-
-// function populateCategory(
-//     elementTitleId,
-//     elementQuestionID,
-//     categoryFilteredArray
-// ) {
-//     // define variables and get the DOM element 
-//     let elementTitle = document.getElementById(elementTitleId);
-//     let title = categoryFilteredArray[0].category;
-//     elementTitle.textContent = title;
-
-//     let questions = document.querySelectorAll(elementQuestionID);
-//     questions.forEach((item, index) => {
-//         item.innerHTML = `${categoryFilteredArray[index].question}  <input type="text" class="answer-${title
-//             .split(" ")
-//             .join("-")
-//             .toLowerCase()}" style="display:block"/>`;
-//     });
-// }
-
-// // Get the modal
-// var modal = document.getElementById("myModal");
-
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-
-// // When the user clicks on the button, open the modal
-// btn.onclick = function () {
-//     modal.style.display = "block";
-// }
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function () {
-//     modal.style.display = "none";
-// }
-
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function (event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
